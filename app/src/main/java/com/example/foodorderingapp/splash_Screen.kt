400eb77f721df62f9051f44a82490a4f2bca08e6
jahanRazh <jahanrazh@gmail.com>
2024-03-09 21:35:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/foodorderingapp/splash_Screen.kt
@@ -0,0 +1,14 @@
+package com.example.foodorderingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        handler().postDelayed({
+            val intent
+        })
+    }
+}
